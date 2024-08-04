dc0e91b2147a101daef98822466f819a4b651865
Jim Tang <wxfreakk91@gmail.com>
2024-08-04 02:39:17 -0500

    Fix checksum

--- weather_cases/extract.py
+++ weather_cases/extract.py
@@ -10,7 +10,7 @@ def to_checksum(row) -> str:
     dt = row["DateTime"]
     lat = row["lat"]
     lon = row["lon"]
-    summary = f'{dt.isoformat()},_{'%.2f' % round(float(lat), 2)},_{'%.2f' % round(float(lon), 2)}'
+    summary = f'{dt.isoformat()}_{'%.2f' % round(float(lat), 2)}_{'%.2f' % round(float(lon), 2)}'
     return hashlib.md5(summary.encode()).hexdigest()
 
 
