3e58aeba330de413ee5773bb421d4034c74a3ab6
Jim Tang <wxfreakk91@gmail.com>
2024-09-13 01:11:51 -0500

    Use 5kt levels again

--- environment/configs.py
+++ environment/configs.py
@@ -14,6 +14,6 @@ CONFIGS = {
     500: Configs(
         level=500,
         height_contours=np.arange(4680, 6100, 60),
-        isotachs=np.arange(20, 141, 1),
+        isotachs=np.arange(20, 141, 5),
     )
 }
