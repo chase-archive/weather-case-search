e98af2335db562f7c7d1d964eee1f93868a358e8
Jim Tang <wxfreakk91@gmail.com>
2024-09-15 23:56:51 -0500

    Write geojsons to S3

--- environment/run.py
+++ environment/run.py
@@ -2,12 +2,10 @@ import pandas as pd
 
 from environment.extents import EXTENTS
 from environment.generate import heights, winds
-from environment.out import filename_of, save_to_file
+from environment.out import save_to_s3
 
 
-def run_for(
-    event_dt: pd.Timestamp, country: str = "US", freq: str = "3H", compress: bool = True
-):
+def run_for(event_dt: pd.Timestamp, country: str = "US", freq: str = "3H"):
     extent = EXTENTS[country]
     if 0 <= event_dt.hour < 12:
         start_dt = (event_dt - pd.Timedelta(1, "d")).replace(hour=0, minute=0, second=0)
@@ -20,17 +18,18 @@ def run_for(
     levels = (500,)
 
     for dt, level, hght_field in heights(extent, analysis_dts, levels):
-        file = filename_of(dt, level, "heights", compress=compress)
-        save_to_file(file, hght_field, compress=compress)
+        save_to_s3(dt, level, "heights", hght_field)
         print(f"Processed heights {level} hPa at {dt}")
 
     for dt, level, isotachs, barbs in winds(extent, analysis_dts, levels):
-        file_isotachs = filename_of(dt, level, "isotachs", compress=compress)
-        file_barbs = filename_of(dt, level, "barbs", compress=compress)
-        save_to_file(file_isotachs, isotachs, compress=compress)
-        save_to_file(file_barbs, barbs, compress=compress)
+        save_to_s3(dt, level, "isotachs", isotachs)
+        save_to_s3(dt, level, "barbs", barbs)
         print(f"Processed wind {level} hPa at {dt}")
 
 
 if __name__ == "__main__":
-    run_for(pd.Timestamp("1999-05-04T00:00:00"), "US", "3H", compress=True)
+    run_for(
+        pd.Timestamp("1999-05-04T00:00:00"),
+        "US",
+        "3H",
+    )
